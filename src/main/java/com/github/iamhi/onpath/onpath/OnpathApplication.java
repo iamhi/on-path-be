056b2295223e7e35ec40703da052b6c051530607
iamhi <inpactt@gmail.com>
2022-03-26 19:30:07 +0100

    Initial

---
+++ src/main/java/com/github/iamhi/onpath/onpath/OnpathApplication.java
@@ -0,0 +1,13 @@
+package com.github.iamhi.onpath.onpath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnpathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnpathApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/iamhi/onpath/onpath/OnpathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.iamhi.onpath.onpath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnpathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
